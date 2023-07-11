f8b6c80641d60e60e2b46385f83e70058838e40d
Godzilla810 <128792917+Godzilla810@users.noreply.github.com>
2023-07-12 05:11:43 +0800

    Add UIResponse
    #要按空白鍵Hit才會動

--- Assets/Scripts/AnimationManager.cs
+++ Assets/Scripts/AnimationManager.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class AnimationManager : MonoBehaviour
 {
     public GameObject devil;
-    public GameObject banana;
+    public GameObject hit;
     public GameObject mainCamera;
     public GameObject subCamera;
 
     private Animator devilAnimator;
+    private Animator hitAnimator;
     private Animator mainCameraAnimator;
     private Animator subCameraAnimator;
 
     private GameManager gameManager;
+
+    private bool isHit;
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameManager.instance;
         devilAnimator = devil.GetComponent<Animator>();
+        hitAnimator = hit.GetComponent<Animator>();
         mainCameraAnimator = mainCamera.GetComponent<Animator>();
         subCameraAnimator = subCamera.GetComponent<Animator>();
     }
@@ -33,9 +37,6 @@ public class AnimationManager : MonoBehaviour
             devilAnimator.SetTrigger("Start");
             mainCameraAnimator.SetTrigger("Transition_ChargeUp");
         }
-        else if (Input.GetKeyDown("space") && gameManager.isChargeUp){
-            
-        }
         else if (gameManager.isFly){
             devilAnimator.SetTrigger("Throw");
             mainCamera.SetActive(false);
@@ -43,6 +44,11 @@ public class AnimationManager : MonoBehaviour
         }
         else if (gameManager.isEnd){
 
+        }
+
+        if (Input.GetKeyDown("space") && gameManager.isChargeUp){
+            hitAnimator.Play("Hit", 0, 0f);
+            // devilAnimator.Play("power", 0, 0f);
         }
     }
     // IEnumerator Countdown_05s(){
